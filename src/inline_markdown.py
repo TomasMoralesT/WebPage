825fcdf096fad236f023b2cae1f5f459ee765143
GNight <tomas11_92@hotmail.com>
2025-07-22 10:18:38 -0500

    "Come on..."

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -5,11 +5,16 @@ from textnode import TextNode, TextType
 
 def text_to_textnodes(text):
     nodes = [TextNode(text, TextType.TEXT)]
-    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
-    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
-    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    # Split images FIRST
     nodes = split_nodes_image(nodes)
+    # Then links
     nodes = split_nodes_link(nodes)
+    # Then code spans
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    # Bold before italics for markdown rules
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    # Now italics
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
     return nodes
 
 
